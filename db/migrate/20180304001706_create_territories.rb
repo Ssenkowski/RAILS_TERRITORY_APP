26bd427d26e8dea82861653c8ae08ef86583a9d2
github username <github email address>
2018-03-03 19:19:23 -0500

    generated territory model with migration columns and types

---
+++ db/migrate/20180304001706_create_territories.rb
@@ -0,0 +1,17 @@
+class CreateTerritories < ActiveRecord::Migration[5.1]
+  def change
+    create_table :territories do |t|
+      t.integer :number
+      t.string :designation
+      t.string :street_names
+      t.date :sign_out
+      t.string :publisher_name
+      t.boolean :campaign_work?
+      t.integer :house_to_house_record_id
+      t.integer :do_not_call_id
+      t.date :last_completed
+
+      t.timestamps
+    end
+  end
+end
