dd19fae6dd52e80a199fea47cb849883dee539b9
github username <github email address>
2018-03-03 19:33:23 -0500

    generated do_not_call model and migration columns and types

---
+++ app/models/do_not_call.rb
@@ -0,0 +1,2 @@
+class DoNotCall < ApplicationRecord
+end
--- db/migrate/20180304002626_create_house_to_house_records.rb
+++ db/migrate/20180304002626_create_house_to_house_records.rb
@@ -1,9 +1,11 @@
 class CreateHouseToHouseRecords < ActiveRecord::Migration[5.1]
   def change
     create_table :house_to_house_records do |t|
+      t.integer :territory_id
       t.integer :address
-      t.string :streetname
+      t.string :street
       t.string :follow_up?
+      t.boolean :do_not_call?, default :false
       t.text :notes
 
       t.timestamps
