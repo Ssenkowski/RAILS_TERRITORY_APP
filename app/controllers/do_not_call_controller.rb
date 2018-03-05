17c851db33ae4d16899ff300bf78cc79b728ccd7
github username <github email address>
2018-03-05 14:39:56 -0500

    outlining the do_not_call controller actions and redirects

--- app/controllers/do_not_call_controller.rb
+++ app/controllers/do_not_call_controller.rb
@@ -1,2 +1,24 @@
 class DoNotCallController < ApplicationController
+
+  def index
+    @do_not_calls = Do_Not_Call.all
+    #List all the do not calls grouped by territory and orderd by address.
+  end
+
+  def create
+    @do_not_call = Do_Not_Call.create(do_not_call_params)
+
+    redirect_to 'do_not_call/index'
+  end
+
+  def show
+    @territory = Territory.find_by_id(params[:id])
+    #Find the do_not_calls through the territory.
+  end
+
+  private
+
+  def do_not_call_params
+      params[:do_not_call].permit(:territory_id, :address, :street, :note, :date)
+  end
 end
