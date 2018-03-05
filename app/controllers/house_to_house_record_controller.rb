e48b5fe64d2e4f3f78deb1a466c34edc7843003c
github username <github email address>
2018-03-05 15:09:58 -0500

    outlining house_to_house_record controller actions and basic functions with strong params

--- app/controllers/house_to_house_record_controller.rb
+++ app/controllers/house_to_house_record_controller.rb
@@ -1,2 +1,26 @@
 class HouseToHouseRecordController < ApplicationController
+  def index
+    @house_to_house_records = House_to_house_record.all
+    #List all the do not calls grouped by territory and orderd by address.
+  end
+
+  def create
+    @house_to_house_record = House_to_house_record.create(house_to_house_record_params)
+    @territory = Territory.find_by_id(params[:territory_id])
+    #Create the house_to_house_record and redirect to the associated do not calls territory.
+
+    redirect_to "house_to_house_record/#{@house_to_house_record.id}"
+  end
+
+  def show
+    @house_to_house_record = House_to_house_record.find_by_id(params[:id])
+    @territory = Territory.find_by_id(params[:territory_id])
+  end
+
+  private
+
+  def house_to_house_record_params
+      params[:house_to_house_record].permit(:territory_id, :publisher_id, :address, :street, :note_symbol, :do_not_call?, :notes, :date)
+  end
+end
 end
--- db/migrate/20180304002626_create_house_to_house_records.rb
+++ db/migrate/20180304002626_create_house_to_house_records.rb
@@ -2,11 +2,13 @@ class CreateHouseToHouseRecords < ActiveRecord::Migration[5.1]
   def change
     create_table :house_to_house_records do |t|
       t.integer :territory_id
+      t.integer :publisher_id
       t.integer :address
       t.string :street
       t.string :note_symbol
       t.boolean :do_not_call?, default :false
       t.text :notes
+      t.date :date
 
       t.timestamps
     end
