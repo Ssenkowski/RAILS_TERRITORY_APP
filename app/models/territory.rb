2fa0cd3893a67f59b8d63e2fec328ad430b69238
github username <github email address>
2018-03-06 13:38:25 -0500

    removed excess methods

--- app/models/territory.rb
+++ app/models/territory.rb
@@ -2,23 +2,11 @@ class Territory < ApplicationRecord
   belongs_to :congregation
   belongs_to :publisher
 
-  def set_house_to_house_record
-
-  end
-
-  def set_do_not_calls
-
-  end
-
   def sign_out
 
   end
 
   def completed_date
-
-  end
-
-  def campaign_work
 
   end
 
