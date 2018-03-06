50b90d1e21bd2cd2ab4b46f99c96dfbf84155524
github username <github email address>
2018-03-05 19:53:12 -0500

    adding congregation model methods and notes

--- app/models/congregation.rb
+++ app/models/congregation.rb
@@ -4,22 +4,24 @@ class Congregation < ApplicationRecord
   has_many :do_not_call_ids, through: :territories
 
   def add_publisher
-
+    @publisher = Publisher.new
   end
 
   def add_territory
-
+    @territory = Territory.new
   end
 
   def add_meeting_times
-
+    #display these with full day and 12 hour clock
+    @weekend_meeting = weekend_meeting
+    @weekday_meeting = weekday_meeting
   end
 
   def weekend_meeting
-
+    @weekend_meeting #possibly set this equal to a parameter from the new congregations_controller method with a calendar date and time selector
   end
 
   def weekday_meeting
-
+    @weekday_meeting #possibly set this equal to a parameter from the new congregations_controller method with a calendar date and time selector
   end
 end
