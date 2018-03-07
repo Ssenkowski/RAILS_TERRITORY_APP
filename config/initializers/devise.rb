9b11e5d56f92bb8d715c29a0c98bff04664810f6
github username <github email address>
2018-03-07 10:56:19 -0500

    configured omniauth app_id and app_secret, Omniauth Facebook through Devise works

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.omniauth :facebook, ENV['1792521237445983'], ENV['731ca12e142ca6d9e99b4becfe7501fc']
+  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
@@ -277,4 +277,8 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+                                  # App_ID first, App_secret second
+  config.omniauth :facebook, "1792521237445983", "731ca12e142ca6d9e99b4becfe7501fc"
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
-  resources :territories, :publishers, :congregations, :products
+  resources :territories, :publishers, :congregations
 
-  root 'products#index'
+  root 'congregations#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
